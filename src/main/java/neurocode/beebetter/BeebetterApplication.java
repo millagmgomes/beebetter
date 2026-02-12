17d15253ca61dea31a589d34adee9fe80f47ce24
macos <gomesmilla59@gmailcom>
2026-02-12 20:45:25 -0300

    first commit

---
+++ src/main/java/neurocode/beebetter/BeebetterApplication.java
@@ -0,0 +1,13 @@
+package neurocode.beebetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeebetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeebetterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/neurocode/beebetter/BeebetterApplicationTests.java
@@ -0,0 +1,13 @@
+package neurocode.beebetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeebetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
